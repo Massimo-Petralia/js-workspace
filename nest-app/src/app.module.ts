ad247446ab0ee776b530c4ef2372ba2d60a9e335
massimo <massipetra@hotmail.it>
2025-12-27 14:22:02 +0100

    tensorflow(tf.service.ts) I implemented a service registered in app.module that return resolved singleton instance of @tensorflow/tfjs with wasm backend setted up, usable in any others Nest service by injection

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -6,9 +6,8 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { CategoryModule } from './features/categories/category.module';
 import { Category } from './features/categories/category.entity';
-import { TensorFlowTest } from './features/tensor-flow-test/tensor-flow-service';
-import { TfService } from './src/features/tensorflowjs/tf/tf.service';
-import { TfService } from './features/tensorflowjs/tf/tf.service';
+import { TensorFlowTest } from './features/tensor-flow-test/tensor-flow-test.service';
+import { TfService } from './features/tensorflowjs/tf.service';
 
 @Module({
   imports: [
@@ -28,6 +27,6 @@ import { TfService } from './features/tensorflowjs/tf/tf.service';
     CategoryModule,
   ],
   controllers: [AppController],
-  providers: [AppService, TensorFlowTest, TfService],
+  providers: [AppService, TfService, TensorFlowTest],
 })
 export class AppModule {}
